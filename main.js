5df06c4bff9fff0150aa6bd56f78a2d2e0bb96cd
dreamgirlto <38843981+dreamgirlto@users.noreply.github.com>
2020-04-25 19:51:57 -0400

    Add files via upload
    The cards now slide smoothly when opening. A row of cards seems to snap when a card in the row above it opens, but the row will slide smoothly back into place when the aforementioned card closes.  Let's see if we can get it to slide smoothly when the card opens as well. 
    
    Another issue here is that even though the ingredients list isn't visible when the card is closed (its opacity is 0), it's still technically displaing (its display is set to block. Removing the display:none property is what enabled the smooth sliding. I'm going to try and use javascript to add display:none to the ingredients list *after* it's done closing and setting its display to block right before it opens, so that it still slide up and down smoothly.

--- main.js
+++ main.js
@@ -5,15 +5,31 @@ targets.forEach((elem) => {
     if (!e.target.expanded) {
       e.target.expanded = true;
       console.log(e.target.expanded);
-      e.target.children[1].style.height = "70%";
-      e.target.children[1].style.opacity = "1";
-      e.target.style.height = "100%";
+      e.target.children[1].classList.remove("card-content-closed");
+      e.target.children[1].classList.add("card-content-open"); //here, instead of directly manipulating styles, just add and remove classes.
+      e.target.classList.remove("card-closed");
+      e.target.classList.add("card-open");
+      console.log(
+        "**TOGGLE ON:** ",
+        e.target,
+        e.target.classList,
+        e.target.children[1].classList
+      );
     } else if (e.target.expanded) {
       e.target.expanded = false;
       console.log(e.target.expanded);
-      e.target.children[1].style.height = "0";
-      e.target.children[1].style.opacity = "0";
-      e.target.style.height = "100%";
+      e.target.children[1].classList.remove("card-content-open");
+      e.target.children[1].classList.add("card-content-closed"); //here, instead of directly manipulating styles, just add and remove classes.
+      e.target.children[1].classList.remove("card-open");
+      e.target.classList.remove("card-open");
+      e.target.classList.add("card-closed");
+
+      console.log(
+        "**TOGGLE OFF:** ",
+        e.target,
+        e.target.classList,
+        e.target.children[1].classList
+      );
     }
   });
 });
