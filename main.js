5aaa0c7e9bd810045a2a9e88c3549030b9b10d2d
dreamgirlto <38843981+dreamgirlto@users.noreply.github.com>
2020-04-26 17:04:34 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,28 @@
+//To use this in the app, put it all in the componentDidMount method of the recipeCard component
+
 let targets = document.querySelectorAll(".card-content");
 
 targets.forEach((elem) => {
   elem.addEventListener("click", (e) => {
     if (!e.target.expanded) {
       e.target.expanded = true;
-
       Array.from(e.target.children[1].children).forEach((li) => {
         li.style.display = "block";
       });
-
-      console.log(e.target.expanded);
       e.target.children[1].classList.remove("card-content-closed");
-      e.target.children[1].classList.add("card-content-open"); //here, instead of directly manipulating styles, just add and remove classes.
+      e.target.children[1].classList.add("card-content-open");
       e.target.classList.remove("card-closed");
       e.target.classList.add("card-open");
-
-      console.log(
-        "**TOGGLE ON:** ",
-        e.target,
-        e.target.classList,
-        e.target.children,
-        e.target.children[1].classList
-      );
     } else if (e.target.expanded) {
       e.target.expanded = false;
-
       Array.from(e.target.children[1].children).forEach((li) => {
         li.style.display = "none";
       });
-
-      console.log(e.target.expanded);
       e.target.children[1].classList.remove("card-content-open");
-      e.target.children[1].classList.add("card-content-closed"); //here, instead of directly manipulating styles, just add and remove classes.
+      e.target.children[1].classList.add("card-content-closed");
       e.target.children[1].classList.remove("card-open");
       e.target.classList.remove("card-open");
       e.target.classList.add("card-closed");
-
-      console.log(
-        "**TOGGLE OFF:** ",
-        e.target,
-        e.target.classList,
-        e.target.children,
-        e.target.children[1].classList
-      );
     }
   });
 });
