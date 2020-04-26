0c4e8e8a1f1b71c21ffc80376a8a1b19237f562c
dreamgirlto <38843981+dreamgirlto@users.noreply.github.com>
2020-04-26 17:19:47 -0400

    Add files via upload
    Changed the targeting on the event listener. Now the user has to directly click 'show details' for the card to expand.

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 //To use this in the app, put it all in the componentDidMount method of the recipeCard component
 
-let targets = document.querySelectorAll(".card-content");
+let targets = document.querySelectorAll(".show-details");
 
 targets.forEach((elem) => {
   elem.addEventListener("click", (e) => {
     if (!e.target.expanded) {
       e.target.expanded = true;
-      Array.from(e.target.children[1].children).forEach((li) => {
+      Array.from(e.target.parentElement.children[1].children).forEach((li) => {
         li.style.display = "block";
       });
-      e.target.children[1].classList.remove("card-content-closed");
-      e.target.children[1].classList.add("card-content-open");
-      e.target.classList.remove("card-closed");
-      e.target.classList.add("card-open");
+      e.target.parentElement.children[1].classList.remove(
+        "card-content-closed"
+      );
+      e.target.parentElement.children[1].classList.add("card-content-open");
+      e.target.parentElement.classList.remove("card-closed");
+      e.target.parentElement.classList.add("card-open");
     } else if (e.target.expanded) {
       e.target.expanded = false;
-      Array.from(e.target.children[1].children).forEach((li) => {
+      Array.from(e.target.parentElement.children[1].children).forEach((li) => {
         li.style.display = "none";
       });
-      e.target.children[1].classList.remove("card-content-open");
-      e.target.children[1].classList.add("card-content-closed");
-      e.target.children[1].classList.remove("card-open");
-      e.target.classList.remove("card-open");
-      e.target.classList.add("card-closed");
+      e.target.parentElement.children[1].classList.remove("card-content-open");
+      e.target.parentElement.children[1].classList.add("card-content-closed");
+      e.target.parentElement.children[1].classList.remove("card-open");
+      e.target.parentElement.classList.remove("card-open");
+      e.target.parentElement.classList.add("card-closed");
     }
   });
 });
